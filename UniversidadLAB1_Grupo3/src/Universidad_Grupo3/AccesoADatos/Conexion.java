3f85f538d80b1447b26f12dc2979f3fb648b1143
adamulp <adamgoestoargentina@gmail.com>
2024-05-15 19:02:45 -0300

    Cambios de conexion y AlumnoData

--- UniversidadLAB1_Grupo3/src/Universidad_Grupo3/AccesoADatos/Conexion.java
+++ UniversidadLAB1_Grupo3/src/Universidad_Grupo3/AccesoADatos/Conexion.java
@@ -3,8 +3,38 @@ package Universidad_Grupo3.AccesoADatos;
 
 /**
  *
- * @author Hollmann
+ * @author Hollmann, et al.
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class Conexion {
+	private static final String URL="jdbc:mariadb://localhost/";
+	private static final String DB="universidad_grupo3";
+	private static String USUARIO="root";
+	private static String PASSWORD="";
+
+	private static Connection connection;
+
+	public Conexion() {
+		if (connection == null){
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    connection = DriverManager.getConnection(URL + DB +
+                                + "?useLegacyDatetimeCode=false&serverTimezone=UTC"
+				+ "&user=" + USUARIO + "&password=" + PASSWORD);
+
+			}
+		}
+        
+        
+    }
+        
+        
+        
     
+
 }
