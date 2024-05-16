4e577a5c76be27d383ba76f0ecfc0a2eede76cb6
AlexisAllendez <163199846+AlexisAllendez@users.noreply.github.com>
2024-05-16 20:34:43 -0300

    Main

--- UniversidadLAB1_Grupo3/src/universidadlab1_grupo3/UniversidadLAB1_Grupo3.java
+++ UniversidadLAB1_Grupo3/src/universidadlab1_grupo3/UniversidadLAB1_Grupo3.java
@@ -4,6 +4,10 @@
  */
 package universidadlab1_grupo3;
 
+import Universidad_Grupo3.AccesoADatos.*;
+import Universidad_Grupo3.Entidades.*;
+import java.time.LocalDate;
+
 /**
  *
  * @author Hollmann
@@ -15,6 +19,19 @@ public class UniversidadLAB1_Grupo3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        AlumnoData ad = new AlumnoData();
+        LocalDate fecha =  LocalDate.of(2003,02,04);
+        
+        Alumno alu = new Alumno(5, "Allendez","Alexis",fecha,true);
+       
+       ad.guardarAlumno(alu);
+      Alumno alu2 = ad.buscarAlumno(5);
+      
+   
+        if (alu2 == null) {
+                System.out.println(alu2);
+        }
     }
     
 }
